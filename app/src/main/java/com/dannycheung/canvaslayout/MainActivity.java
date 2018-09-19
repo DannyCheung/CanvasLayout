54a565f9cf0a56a409ae648fdbdb872049767408
DannyCheung <DannyCheung@tencent.com>
2018-09-19 15:54:47 +0800

    demo架构提交

--- app/src/main/java/com/dannycheung/canvaslayout/MainActivity.java
+++ app/src/main/java/com/dannycheung/canvaslayout/MainActivity.java
@@ -1,7 +1,14 @@
 package com.dannycheung.canvaslayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.dannycheung.canvaslayout.example.ImageNodeActivity;
+import com.dannycheung.canvaslayout.example.NetImageNodeActivity;
+import com.dannycheung.canvaslayout.example.TextNodeActivity;
+import com.dannycheung.canvaslayout.example.ViewNodeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.viewnode).setOnClickListener(nodeClickListener);
+        findViewById(R.id.textnode).setOnClickListener(nodeClickListener);
+        findViewById(R.id.imagenode).setOnClickListener(nodeClickListener);
+        findViewById(R.id.netimagenode).setOnClickListener(nodeClickListener);
+
     }
+
+    private View.OnClickListener nodeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.viewnode:
+                    startActivity(new Intent(MainActivity.this, ViewNodeActivity.class));
+                    break;
+                case R.id.textnode:
+                    startActivity(new Intent(MainActivity.this, TextNodeActivity.class));
+                    break;
+                case R.id.imagenode:
+                    startActivity(new Intent(MainActivity.this, ImageNodeActivity.class));
+                    break;
+                case R.id.netimagenode:
+                    startActivity(new Intent(MainActivity.this, NetImageNodeActivity.class));
+                    break;
+            }
+        }
+    };
+
+
 }
---
+++ app/src/main/java/com/dannycheung/canvaslayout/example/ImageNodeActivity.java
@@ -0,0 +1,42 @@
+package com.dannycheung.canvaslayout.example;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class ImageNodeActivity extends AppCompatActivity {
+
+    void render() {
+
+    }
+
+    /**
+     * it doesn't matter
+     */
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("ImageNode");
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        //
+        render();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
